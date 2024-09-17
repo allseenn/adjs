02044dcaa2d0e290077c8605109df34dec9a9ba8
allseenn <allseen@live.ru>
2024-09-17 10:11:38 +0000

    first

---
+++ 01.Lecture/1.js
@@ -0,0 +1,9 @@
+let obj = {
+    animal: 'dog',
+}
+
+let obj2 = obj;
+
+obj = null;
+console.log(obj);
+console.log(obj2);
---
+++ 01.Lecture/symbol.js
@@ -0,0 +1,9 @@
+const symbol = Symbol();
+const dogID = Symbol('dog');
+
+const dog1 = Symbol('dog');
+const dog2 = Symbol('dog');
+
+console.log(dog1);
+console.log(dog2);
+console.log(dog1 === dog2);

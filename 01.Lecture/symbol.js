974774de383f91b6ccb3bcb82f44cbbb33a2f896
allseenn <allseen@live.ru>
2024-09-21 11:51:53 +0000

    f

--- 01.Lecture/symbol.js
+++ 01.Lecture/symbol.js
@@ -1,9 +1,17 @@
-const symbol = Symbol();
-const dogID = Symbol('dog');
+// const symbol = Symbol();
+// const dogID = Symbol('dog');
 
-const dog1 = Symbol('dog');
-const dog2 = Symbol('dog');
+// const dog1 = Symbol('dog');
+// const dog2 = Symbol('dog');
 
-console.log(dog1);
-console.log(dog2);
-console.log(dog1 === dog2);
+// console.log(dog1);
+// console.log(dog2);
+// console.log(dog1 === dog2);
+
+// 23 минута лекция 1
+let id = Symbol('Жучка');
+let buddy = {
+};
+console.log(buddy.id);
+buddy.id = 'Бобик';
+console.log(buddy.id); // должен вывести второй раз жучка?
